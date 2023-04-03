98a514e4a998cea101b6f690e49c91956a84c3df
Julia-Gareeva <uliastekolnikova12822@gmail.com>
2023-04-03 10:39:30 +0500

    Исправления от 03.04.23 - 1

---
+++ main_app/serializers/publications.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from main_app.models import Publications, User, Comments
+
+
+class PublicationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publications
+        fields = "__all__"
+
+    author = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field="first_name" + "last_name"
+    )
+
+    comments = serializers.SlugRelatedField(
+        queryset=Comments.objects.all(),
+        slug_field="author" + "text"
+    )
