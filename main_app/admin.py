f1996367cda891fc61fa0349d6a4de2ed568bab8
Julia-Gareeva <uliastekolnikova12822@gmail.com>
2023-04-18 01:15:43 +0500

    Исправления от 18.04.23 - 2

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from main_app.models import User, Publications, Comments
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone_number", "birthday", "date_joined", "date_editing")
+    # Поля которые будут показаны в админке.
+    list_filter = ("first_name", "last_name", "birthday")
+    # Поля по которым будет производиться фильтрация в админке.
+    search_fields = ("first_name", "last_name", "full_name")
+    # Поля по которым будет происходить поиск в админке.
+
+
+class PublicationsAdmin(admin.ModelAdmin):
+    list_display = ("heading", "text", "image", "author_link", "comments", "date_creation", "date_editing")
+    list_filter = ("heading", "author_link", "text", "date_creation")
+    search_fields = ("heading", "text", "author_link")
+
+    def author_link(self, obj):
+        author = obj.author
+        url = reverse("admin:main_app_user_changelist") + str(author.pk)
+        return format_html(f"<a href='{url}'>{author}</a>")
+
+    author_link.short_description = "Автор"
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("author_link", "text", "date_creation", "date_editing")
+    list_filter = ("author_link", "text")
+    search_fields = ("author_link", "text")
+
+    def author_link(self, obj):
+        author = obj.author
+        url = reverse("admin:main_app_user_changelist") + str(author.pk)
+        return format_html(f"<a href='{url}'>{author}</a>")
+
+    author_link.short_description = "Автор"
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Publications, PublicationsAdmin)
+admin.site.register(Comments, CommentsAdmin)
